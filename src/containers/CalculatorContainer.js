a74ce76dbfc014ccd29f7d0ecddfd1c0266e7ac0
Cath Riddoch <cathriddoch@gmail.com>
2020-02-04 10:30:27 +0000

    styles the calculator component

--- src/containers/CalculatorContainer.js
+++ src/containers/CalculatorContainer.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import CalcButtons from '../components/CalcButtons'
 
 class CalculatorContainer extends Component {
